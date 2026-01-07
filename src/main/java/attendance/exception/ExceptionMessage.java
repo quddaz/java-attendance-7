57cf199f2a9e0e8c99df2957ff1260b51440e2e5
김병웅 <quddnddl35@naver.com>
2026-01-07 20:29:08 +0900

    feat : 기본 exception 에러문 정의

--- src/main/java/attendance/exception/ExceptionMessage.java
+++ src/main/java/attendance/exception/ExceptionMessage.java
@@ -1,7 +1,12 @@
 package attendance.exception;
 
 public enum ExceptionMessage {
-    INVALID_FORMAT_NUMBER("[ERROR] 구매 금액은 숫자여야 합니다.");
+    INVALID_FORMAT("[ERROR] 잘못된 형식을 입력하였습니다."),
+    NOT_FOUNT_NAME("[ERROR] 등록되지 않은 닉네임입니다."),
+    NON_WEEKDAY("[ERROR] %d월 %d일 %s은 등교일이 아닙니다.\n"),
+    CHANGE_ERROR("[ERROR] 아직 수정할 수 없습니다."),
+    NO_CHANGE_TIME("[ERROR] 캠퍼스 운영 시간에만 출석이 가능합니다."),
+    DUPLICATE_ATTENDANCE("[ERROR] 이미 출석을 확인하였습니다. 필요한 경우 수정 기능을 이용해 주세요.");
 
     private final String message;
 
